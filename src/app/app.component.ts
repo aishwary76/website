410e69b9e955d30eace6315eeb4902b0b123e04c
aishwary76 <aishwary76@gmail.com>
2020-06-08 22:46:28 +0530

    first

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'website';
+//   openSlider() {
+//     document.getElementById('side-menu').style.width = '250px';
+//     document.getElementById('main').style.marginLeft = '250px';
+//     console.log('working');
+// }
+// closeSlider() {
+//     document.getElementById('side-menu').style.width = '0px';
+//     document.getElementById('main').style.marginLeft = '0px';
+// }
+}
