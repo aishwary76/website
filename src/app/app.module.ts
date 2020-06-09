6d78e3a66d672b7e7edc4f5891c1aef2dad4013b
aishwary76 <aishwary76@gmail.com>
2020-06-09 13:53:47 +0530

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { FoundersComponent } from './founders/founders.component';
+import { BlogComponent } from './blog/blog.component';
+import { ClientsComponent } from './clients/clients.component';
+import { LandingComponent } from './landing/landing.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    FoundersComponent,
+    BlogComponent,
+    ClientsComponent,
+    LandingComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
