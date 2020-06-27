b5bea9edaf8bd8a88888f605edc92862bf99b26b
amolsr <amol.saini567@gmail.com>
2020-06-28 01:16:54 +0530

    team section added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
-import { FoundersComponent } from './founders/founders.component';
-import { BlogComponent } from './blog/blog.component';
-import { ClientsComponent } from './clients/clients.component';
-import { LandingComponent } from './landing/landing.component';
-import { FooterComponent } from './footer/footer.component';
-import { SkillsComponent } from './skills/skills.component';
-import { AboutComponent } from './about/about.component';
-import { StrategyComponent } from './strategy/strategy.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavComponent } from "./nav/nav.component";
+import { FoundersComponent } from "./founders/founders.component";
+import { BlogComponent } from "./blog/blog.component";
+import { ClientsComponent } from "./clients/clients.component";
+import { LandingComponent } from "./landing/landing.component";
+import { FooterComponent } from "./footer/footer.component";
+import { SkillsComponent } from "./skills/skills.component";
+import { AboutComponent } from "./about/about.component";
+import { StrategyComponent } from "./strategy/strategy.component";
+import { PortfolioComponent } from "./portfolio/portfolio.component";
+import { TeamComponent } from "./team/team.component";
 
 @NgModule({
   declarations: [
@@ -20,19 +21,17 @@ import { PortfolioComponent } from './portfolio/portfolio.component';
     NavComponent,
     FoundersComponent,
     BlogComponent,
+    TeamComponent,
     ClientsComponent,
     LandingComponent,
     FooterComponent,
     SkillsComponent,
     AboutComponent,
     StrategyComponent,
-    PortfolioComponent
+    PortfolioComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
