64a9dc4244bd42173accf5ae6c93df165c0da4f9
amolsr <amol.saini567@gmail.com>
2020-06-29 00:56:46 +0530

    error handling in chatbot

--- src/app/chatbot/chat.component.ts
+++ src/app/chatbot/chat.component.ts
@@ -23,7 +23,7 @@ export class ChatComponent implements OnInit {
     $("#send").click(function (e) {
       e.preventDefault();
       var msg = $("input[name='message']").val();
-
+      console.log(msg);
       if (!(msg == "")) {
         var dm = new Date();
         $(".Messages_list").append(
@@ -54,10 +54,26 @@ export class ChatComponent implements OnInit {
             $(".Messages").scrollTop($(".Messages").prop("scrollHeight"));
           })
           .fail(function (jqXHR, textStatus) {
-            alert(textStatus);
+            var d = new Date();
+            $(".Messages_list").append(
+              "<div class='incoming_msg'><div style='display: inline-block; width: 10%;'><img src='https://ptetutorials.com/images/user-profile.png' style='max-width:100%;' alt='sunil'> </div> <div style='display: inline-block; padding: 0 0 0 10px; vertical-align: top; width: 85%;'>  <div style='width: 57%;'>  <p style='  background: #ebebeb none repeat scroll 0 0; border-radius: 3px; color: #646464; font-size: 14px; margin: 0; padding: 5px 10px 5px 12px; width: 100%;'>Sorry I am feeling ill.</p><span style='color: #747474; display: block; font-size: 12px; margin: 8px 0 0;'>" +
+                d.getHours() +
+                ":" +
+                d.getMinutes() +
+                "</span></div></div></div>"
+            );
+            $(".Messages").scrollTop($(".Messages").prop("scrollHeight"));
           });
       } else {
-        alert("empty");
+        var d = new Date();
+        $(".Messages_list").append(
+          "<div class='incoming_msg'><div style='display: inline-block; width: 10%;'><img src='https://ptetutorials.com/images/user-profile.png' style='max-width:100%;' alt='sunil'> </div> <div style='display: inline-block; padding: 0 0 0 10px; vertical-align: top; width: 85%;'>  <div style='width: 57%;'>  <p style='  background: #ebebeb none repeat scroll 0 0; border-radius: 3px; color: #646464; font-size: 14px; margin: 0; padding: 5px 10px 5px 12px; width: 100%;'>Nothing to Say?</p><span style='color: #747474; display: block; font-size: 12px; margin: 8px 0 0;'>" +
+            d.getHours() +
+            ":" +
+            d.getMinutes() +
+            "</span></div></div></div>"
+        );
+        $(".Messages").scrollTop($(".Messages").prop("scrollHeight"));
       }
     });
   }
