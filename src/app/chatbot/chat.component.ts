43fe4a6b3f5245555eedc101e49d4e4fedcebffd
amolsr <amol.saini567@gmail.com>
2020-06-29 01:23:59 +0530

    text process

--- src/app/chatbot/chat.component.ts
+++ src/app/chatbot/chat.component.ts
@@ -26,7 +26,7 @@ export class ChatComponent implements OnInit {
       if (!(msg == "")) {
         var dm = new Date();
         $(".Messages_list").append(
-          "<div style='overflow: hidden; margin: 26px 0 26px;'><div style=' float: right; width: 46%;'><p style='background: #05728f none repeat scroll 0 0; border-radius: 3px; font-size: 14px; margin: 0; color: #fff; padding: 5px 10px 5px 12px; width: 100%;'>" +
+          "<div style='overflow: hidden; margin: 26px 0 26px;'><div style=' float: right; width: 46%;'><p style='background: #05728f none repeat scroll 0 0; border-radius: 3px; font-size: 14px; margin: 0; color: #fff; padding: 5px 10px 5px 12px; width: 100%; text-transform: capitalize;'>" +
             msg +
             "</p><span style='color: #747474; display: block; font-size: 12px; margin: 8px 0 0;'>" +
             dm.getHours() +
@@ -43,7 +43,7 @@ export class ChatComponent implements OnInit {
           .done(function (data) {
             var d = new Date();
             $(".Messages_list").append(
-              "<div class='incoming_msg'><div style='display: inline-block; width: 10%;'><img src='https://ptetutorials.com/images/user-profile.png' style='max-width:100%;' alt='sunil'> </div> <div style='display: inline-block; padding: 0 0 0 10px; vertical-align: top; width: 85%;'>  <div style='width: 57%;'>  <p style='  background: #ebebeb none repeat scroll 0 0; border-radius: 3px; color: #646464; font-size: 14px; margin: 0; padding: 5px 10px 5px 12px; width: 100%;'>" +
+              "<div class='incoming_msg'><div style='display: inline-block; width: 10%;'><img src='https://ptetutorials.com/images/user-profile.png' style='max-width:100%;' alt='sunil'> </div> <div style='display: inline-block; padding: 0 0 0 10px; vertical-align: top; width: 85%;'>  <div style='width: 57%;'>  <p style='  background: #ebebeb none repeat scroll 0 0; border-radius: 3px; color: #646464; font-size: 14px; margin: 0; padding: 5px 10px 5px 12px; width: 100%; text-transform: capitalize;'>" +
                 data.response +
                 "</p><span style='color: #747474; display: block; font-size: 12px; margin: 8px 0 0;'>" +
                 d.getHours() +
@@ -56,7 +56,7 @@ export class ChatComponent implements OnInit {
           .fail(function (jqXHR, textStatus) {
             var d = new Date();
             $(".Messages_list").append(
-              "<div class='incoming_msg'><div style='display: inline-block; width: 10%;'><img src='https://ptetutorials.com/images/user-profile.png' style='max-width:100%;' alt='sunil'> </div> <div style='display: inline-block; padding: 0 0 0 10px; vertical-align: top; width: 85%;'>  <div style='width: 57%;'>  <p style='  background: #ebebeb none repeat scroll 0 0; border-radius: 3px; color: #646464; font-size: 14px; margin: 0; padding: 5px 10px 5px 12px; width: 100%;'>Sorry I am feeling ill.</p><span style='color: #747474; display: block; font-size: 12px; margin: 8px 0 0;'>" +
+              "<div class='incoming_msg'><div style='display: inline-block; width: 10%;'><img src='https://ptetutorials.com/images/user-profile.png' style='max-width:100%;' alt='sunil'> </div> <div style='display: inline-block; padding: 0 0 0 10px; vertical-align: top; width: 85%;'>  <div style='width: 57%;'>  <p style='  background: #ebebeb none repeat scroll 0 0; border-radius: 3px; color: #646464; font-size: 14px; margin: 0; padding: 5px 10px 5px 12px; width: 100%;'>Sorry I am feeling ill.</p><span style='color: #747474; display: block; font-size: 12px; margin: 8px 0 0; text-transform: capitalize;'>" +
                 d.getHours() +
                 ":" +
                 d.getMinutes() +
@@ -67,7 +67,7 @@ export class ChatComponent implements OnInit {
       } else {
         var d = new Date();
         $(".Messages_list").append(
-          "<div class='incoming_msg'><div style='display: inline-block; width: 10%;'><img src='https://ptetutorials.com/images/user-profile.png' style='max-width:100%;' alt='sunil'> </div> <div style='display: inline-block; padding: 0 0 0 10px; vertical-align: top; width: 85%;'>  <div style='width: 57%;'>  <p style='  background: #ebebeb none repeat scroll 0 0; border-radius: 3px; color: #646464; font-size: 14px; margin: 0; padding: 5px 10px 5px 12px; width: 100%;'>Nothing to Say?</p><span style='color: #747474; display: block; font-size: 12px; margin: 8px 0 0;'>" +
+          "<div class='incoming_msg'><div style='display: inline-block; width: 10%;'><img src='https://ptetutorials.com/images/user-profile.png' style='max-width:100%;' alt='sunil'> </div> <div style='display: inline-block; padding: 0 0 0 10px; vertical-align: top; width: 85%;'>  <div style='width: 57%;'>  <p style='  background: #ebebeb none repeat scroll 0 0; border-radius: 3px; color: #646464; font-size: 14px; margin: 0; padding: 5px 10px 5px 12px; width: 100%;'>Nothing to Say?</p><span style='color: #747474; display: block; font-size: 12px; margin: 8px 0 0; text-transform: capitalize;'>" +
             d.getHours() +
             ":" +
             d.getMinutes() +
